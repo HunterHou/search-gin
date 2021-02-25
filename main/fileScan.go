bbc47aeb725663831f3683d09e7a54f6ca57b76f
HunterHou <183598821@qq.com>
2021-02-25 16:51:16 +0800

    hd

--- main/fileScan.go
+++ main/fileScan.go
@@ -2,21 +2,19 @@ package main
 
 import (
 	"fmt"
-	"testing"
-
 	"search-gin/cons"
 	"search-gin/service"
 	"search-gin/utils"
 )
 
 // TestScan ..
-func TestScan(test testing.T) {
+func main() {
 
-	var baseDir = "f:\\"
-	var videoTypes = []string{cons.AVI, cons.MKV, cons.WMV, cons.MP4}
-	// var imageTypes = []string{cons.JPG, cons.PNG, cons.GIF}
+	var baseDir = "d:\\emby"
+	//var videoTypes = []string{cons.AVI, cons.MKV, cons.WMV, cons.MP4}
+	var imageTypes = []string{cons.JPG, cons.PNG, cons.GIF}
 	var queryTypes []string
-	queryTypes = utils.ExtandsItems(queryTypes, videoTypes)
+	queryTypes = utils.ExtandsItems(queryTypes, imageTypes)
 	items := service.Walk(baseDir, queryTypes)
 	i := 0
 	for {
