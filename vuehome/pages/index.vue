c73b3bab4d3daf1c35417e81b245c279747e2747
HunterHou <183598821@qq.com>
2021-05-13 20:36:47 +0800

    hd

--- vuehome/pages/index.vue
+++ vuehome/pages/index.vue
@@ -4,52 +4,7 @@
     <div id="msg">
     </div>
     <div>
-      <!-- <Logo />
-      <h1 class="title">
-        vuehome
-      </h1> -->
- <el-container>
-
-        <el-container>
-            <el-header>
-                <el-menu router :default-active="'/home'" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-                    <el-menu-item index="/home">
-                        <i class="el-icon-document"></i>
-                        <span slot="title"></span>
-                    </el-menu-item>
-                    <el-menu-item index="/menu/1">
-                        <i class="el-icon-menu"></i>
-                        <span slot="title">目录</span>
-                    </el-menu-item>
-                    <el-menu-item index="/context">
-                        <i class="el-icon-location"></i
-                        ></el-menu-item>
-                    <el-menu-item index="/setting">
-                        <i class="el-icon-setting"></i>
-                        <span slot="title">设置</span>
-                    </el-menu-item>
-                </el-menu>
-            </el-header>
-                <el-main>
-                    <router-view></router-view>
-                </el-main>
-            <el-backtop :bottom="100" style="width: 50px; height: 50px;">
-                <div
-                        style="
-              height: 100%;
-              width: 100%;
-              background-color: #f2f5f6;
-              box-shadow: 0 0 6px rgba(0, 0, 0, 0.12);
-              text-align: center;
-              line-height: 40px;
-              color: #1989fa;
-            "
-                >
-                    UP
-                </div>
-            </el-backtop>
-        </el-container>
-    </el-container>
+ 11
     </div>
   </div>
 </template>
