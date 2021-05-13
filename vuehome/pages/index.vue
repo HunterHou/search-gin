a1d5e61172193af31a0918e4c59575c55e08ac66
HunterHou <183598821@qq.com>
2021-05-13 20:26:24 +0800

    hd

--- vuehome/pages/index.vue
+++ vuehome/pages/index.vue
@@ -1,28 +1,55 @@
 <template>
+
   <div class="container">
+    <div id="msg">
+    </div>
     <div>
-      <Logo />
+      <!-- <Logo />
       <h1 class="title">
         vuehome
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      </h1> -->
+ <el-container>
+
+        <el-container>
+            <el-header>
+                <el-menu router :default-active="'/home'" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                    <el-menu-item index="/home">
+                        <i class="el-icon-document"></i>
+                        <span slot="title"></span>
+                    </el-menu-item>
+                    <el-menu-item index="/menu/1">
+                        <i class="el-icon-menu"></i>
+                        <span slot="title">目录</span>
+                    </el-menu-item>
+                    <el-menu-item index="/context">
+                        <i class="el-icon-location"></i
+                        ></el-menu-item>
+                    <el-menu-item index="/setting">
+                        <i class="el-icon-setting"></i>
+                        <span slot="title">设置</span>
+                    </el-menu-item>
+                </el-menu>
+            </el-header>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            <el-backtop :bottom="100" style="width: 50px; height: 50px;">
+                <div
+                        style="
+              height: 100%;
+              width: 100%;
+              background-color: #f2f5f6;
+              box-shadow: 0 0 6px rgba(0, 0, 0, 0.12);
+              text-align: center;
+              line-height: 40px;
+              color: #1989fa;
+            "
+                >
+                    UP
+                </div>
+            </el-backtop>
+        </el-container>
+    </el-container>
     </div>
   </div>
 </template>
