826e7f71add0f9278515eba2b21bcbc5f63fefc5
HunterHou <183598821@qq.com>
2021-05-14 17:20:58 +0800

    hd

--- router/BuildRouter.go
+++ router/BuildRouter.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"net/http"
-
 	"search-gin/controller"
 
 	"github.com/gin-gonic/gin"
@@ -10,30 +9,32 @@ import (
 
 func BuildRouter() *gin.Engine {
 	router := gin.Default()
-	router.StaticFS("/static", http.Dir("static"))
-	router.LoadHTMLFiles("static/index.html")
+	// router.StaticFS("/static", http.Dir("static"))
+	router.StaticFS("/_nuxt", http.Dir("./vuehome/dist/_nuxt"))
+	router.LoadHTMLFiles("./vuehome/dist/index.html")
+	// router.LoadHTMLGlob("vuehome/dist/*")
 
 	router.GET("/", controller.Index)
-	router.POST("api/movieList", controller.PostMovies)
-	router.GET("api/refresh", controller.GetFresh)
-	router.GET("api/actressList", controller.GetActess)
+	router.POST("/api/movieList", controller.PostMovies)
+	router.GET("/api/refresh", controller.GetFresh)
+	router.GET("/api/actressList", controller.GetActess)
 
-	router.GET("api/play/:id", controller.GetPlay)
-	router.GET("api/setMovieType/:id/:movieType", controller.SetMovieType)
-	router.GET("api/info/:id", controller.GetInfo)
-	router.GET("api/delete/:id", controller.GetDelete)
-	router.GET("api/sync/:id", controller.GetSync)
-	router.GET("api/openFolder/:id", controller.GetOpenFoler)
+	router.GET("/api/play/:id", controller.GetPlay)
+	router.GET("/api/setMovieType/:id/:movieType", controller.SetMovieType)
+	router.GET("/api/info/:id", controller.GetInfo)
+	router.GET("/api/delete/:id", controller.GetDelete)
+	router.GET("/api/sync/:id", controller.GetSync)
+	router.GET("/api/openFolder/:id", controller.GetOpenFoler)
 
-	router.GET("api/infoNext/:id", controller.GetNextInfo)
-	router.GET("api/infoLast/:id", controller.GetLastInfo)
+	router.GET("/api/infoNext/:id", controller.GetNextInfo)
+	router.GET("/api/infoLast/:id", controller.GetLastInfo)
 
-	router.GET("api/png/:path", controller.GetPng)
-	router.GET("api/jpg/:path", controller.GetJpg)
+	router.GET("/api/png/:path", controller.GetPng)
+	router.GET("/api/jpg/:path", controller.GetJpg)
 
-	router.GET("api/buttoms", controller.GetButtom)
-	router.GET("api/refreshIndex", controller.GetRefresIndex)
-	router.GET("api/settingInfo", controller.GetSettingInfo)
-	router.POST("api/setting", controller.PostSetting)
+	router.GET("/api/buttoms", controller.GetButtom)
+	router.GET("/api/refreshIndex", controller.GetRefresIndex)
+	router.GET("/api/settingInfo", controller.GetSettingInfo)
+	router.POST("/api/setting", controller.PostSetting)
 	return router
 }
