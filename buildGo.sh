8e5afa35ba22394ef25aed26fef938b30aba5166
HunterHou <183598821@qq.com>
2021-11-22 15:26:36 +0800

    hd

--- buildGo.sh
+++ buildGo.sh
@@ -1,16 +1,45 @@
-cd ./app  
-rm -rf vuehome 
-mkdir -p vuehome/dist 
-cd .. 
-cd vuehome 
-echo 开始打包前端文件 
-npm run generate 
-echo 打包完成，开始移动到app目录 
-cp -R dist  ../app/vuehome 
-cd .. 
-echo 移动完成，打包APP 
-go build -o app/appVue.exe -ldflags "-H=windowsgui" 
-echo 移动配置文件 目标名称 '*(1)' 
-cp dirList.ini 'app/dirList.ini(1)' 
-echo APP打包完成！！！ 
-cp -R app election/src
+#!/bin/bash
+levelKey=${1}
+printf '执行参数：levelKey[{%s}] \n' $levelKey 
+levelValue=''
+if [ $levelKey -eq 0 ]; then
+    levelKey=2
+    levelValue='vueGo'
+elif [ $levelKey -eq 1 ]; then
+    levelValue='vue'
+elif [ $levelKey -eq 2 ]; then
+    levelValue='vueGo'
+elif [ $levelKey -eq 3 ]; then
+    levelValue='vueGoElection'
+fi
+printf '执行级别：levelKey[{%s}]--levelValue[{%s}] \n' $levelKey $levelValue
+echo 'cd ./app'
+cd ./app
+echo 'rm -rf vuehome'
+rm -rf vuehome
+echo 'mkdir -p vuehome/dist'
+mkdir -p vuehome/dist
+echo 'cd ..'
+cd ..
+echo 'cd vuehome'
+cd vuehome
+echo 开始打包前端文件
+npm run generate
+echo 打包完成，开始移动到app目录
+echo 'cp -R dist ../app/vuehome'
+cp -R dist ../app/vuehome
+cd ..
+if [ $levelKey -ge 2 ]; then
+    echo 移动完成，打包APP
+    echo 'go build'
+    go build -o app/appVue.exe -ldflags "-H=windowsgui"
+    echo 移动配置文件 '*.*(1)'
+    echo 'cp dirList.ini app/dirList.ini(1)'
+    cp dirList.ini 'app/dirList.ini(1)'
+    echo APP打包完成！！！
+fi
+if [ $levelKey -ge 3 ]; then
+    echo 移动源到 Election代码目录
+    echo 'cp -R app election/src'
+    cp -R app election/src
+fi
