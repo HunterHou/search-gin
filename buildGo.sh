9a0e21a672b06d7941488c4f7e5f1980930ab7e4
HunterHou <183598821@qq.com>
2021-11-22 15:37:31 +0800

    hd

--- buildGo.sh
+++ buildGo.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 levelKey=${1}
-printf '执行参数：levelKey[{%s}] \n' $levelKey 
+printf '执行参数：levelKey[{%s}] \n' $levelKey
 levelValue=''
-if [ $levelKey -eq 0 ]; then
+if [ !$levelKey ]; then
     levelKey=2
     levelValue='vueGo'
+elif [ $levelKey -eq 0 ]; then
+    levelKey=0
+    levelValue='none'
 elif [ $levelKey -eq 1 ]; then
     levelValue='vue'
 elif [ $levelKey -eq 2 ]; then
@@ -23,15 +26,19 @@ echo 'cd ..'
 cd ..
 echo 'cd vuehome'
 cd vuehome
-echo 开始打包前端文件
-npm run generate
-echo 打包完成，开始移动到app目录
-echo 'cp -R dist ../app/vuehome'
-cp -R dist ../app/vuehome
+if [ $levelKey - ge 1 ]; then
+
+    echo 开始打包前端文件
+    echo 'npm run generate'
+    npm run generate
+    echo 打包完成，开始移动到app目录
+    echo 'cp -R dist ../app/vuehome'
+    cp -R dist ../app/vuehome
+fi
 cd ..
 if [ $levelKey -ge 2 ]; then
     echo 移动完成，打包APP
-    echo 'go build'
+    echo 'go build -o app/appVue.exe -ldflags "-H=windowsgui" '
     go build -o app/appVue.exe -ldflags "-H=windowsgui"
     echo 移动配置文件 '*.*(1)'
     echo 'cp dirList.ini app/dirList.ini(1)'
@@ -42,4 +49,9 @@ if [ $levelKey -ge 3 ]; then
     echo 移动源到 Election代码目录
     echo 'cp -R app election/src'
     cp -R app election/src
+
 fi
+echo ''
+echo 'success,over !!!'
+echo 'success,over !!!'
+echo 'success,over !!!'
