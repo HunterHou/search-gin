7253db1263e1fa8bda8fcd2d2de5ca82cdf4f188
HunterHou <183598821@qq.com>
2022-01-26 14:15:41 +0800

    hd

--- vite/src/App.vue
+++ vite/src/App.vue
@@ -1,21 +1,40 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import MenuTool from "./components/MenuTool.vue";
+import Home from "./components/Home.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="common-layout">
+   <el-container>
+      <el-header>
+        <MenuTool></MenuTool>
+      </el-header>
+      <el-main>
+        <Home/>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less">
+.common-layout {
+  .el-header {
+    // background-color: #b3c0d1;
+    // color: var(--el-text-color-primary);
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-main {
+    // background-color: #e9eef3;
+    // color: var(--el-text-color-primary);
+    text-align: center;
+    line-height: 160px; 
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
 }
 </style>
