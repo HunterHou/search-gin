982640b90a6dbd20d738ea4c4ae44ce42c22f50b
hunter.hou <183598821@qq.com>
2022-08-01 01:40:52 +0800

    hd

--- viteHome/vite.config.ts
+++ viteHome/vite.config.ts
@@ -1,7 +1,8 @@
-const path =require('path')
+
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from 'path'
 
 export default defineConfig({
     plugins: [vue()],
