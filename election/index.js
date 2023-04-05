ee99d57ad31acf4bb32b97ce4bf1a2bdd65045de
hunter <18358821@qq.com>
2023-04-05 20:23:42 +0800

    1

--- election/index.js
+++ election/index.js
@@ -1,36 +1,61 @@
-const {app, BrowserWindow,process,remote} = require('electron')
-// const MenuItem = remote.MenuItem;
-// const Menu = remote.Menu;
-let syetem = require('child_process')
+const { startBg, killBg } = require("./src/js/exec");
+const { app, BrowserWindow, process, Menu, MenuItem } = require("electron");
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 1400,
-        height: 900
-    })
-    return win
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+  });
+  return win;
 }
 
-
-// var menu = new Menu();
-// menu.append(new MenuItem({ label: 'MenuItem1', click: function() { console.log('item 1 clicked'); } }));
-// menu.append(new MenuItem({ type: 'separator' }));
-// menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }));
-
-
+var menu = new Menu();
+menu.append(
+  new MenuItem({
+    label: "打开",
+    click: function () {
+      console.log("打开");
+    },
+  })
+);
+menu.append(new MenuItem({ type: "separator" }));
+menu.append(
+  new MenuItem({
+    label: "设置",
+    type: "checkbox",
+    checked: true,
+    click: () => {
+      console.log("设置");
+    },
+  })
+);
+menu.append(new MenuItem({ type: "separator" }));
+menu.append(
+  new MenuItem({
+    label: "关闭",
+    type: "checkbox",
+    checked: true,
+    click: () => {
+      killBg();
+      app.quit();
+    },
+  })
+);
 
 app.whenReady().then(() => {
-    syetem.exec("cd src && cd app && start appVue.exe && cd .. && cd ..")
-    win = createWindow()
-    // win.addEventListener('contextmenu', function (e) {
-    //     e.preventDefault();
-    //     menu.popup(remote.getCurrentWindow());
-    //   }, false);
-    // syetem.exec("C:\\Windows\\System32\\cmd.exe start ./app/appVue.exe")
-    win.loadURL("http://127.0.0.1:10801/home")
-    app.on('window-all-closed', function () {
-        if (process.platform !== 'darwin') app.quit()
-    })
-    app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+  win = createWindow();
+  Menu.setApplicationMenu(menu);
+  win.loadFile("index.html");
+  startBg((err, stdout, stderr) => {
+    if (err) {
+      console.log("startBg", err);
+      return;
+    }
+    win.loadURL("http://127.0.0.1:10081/home");
+  });
+  app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") app.quit();
+  });
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
