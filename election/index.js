ee28d0e2f6f8c4cba4bc38a82ef21b7e869c4589
hunterPC <hunter@hnter>
2023-04-05 23:34:14 +0800

    1

--- election/index.js
+++ election/index.js
@@ -1,5 +1,15 @@
 const sys = require("child_process");
-const { app, BrowserWindow, process, Menu, MenuItem } = require("electron");
+const process = require("node:process");
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  Tray,
+  MenuItem,
+  nativeImage,
+} = require("electron");
+
+const bgUrl = "http://127.0.0.1:10081/home";
 function createWindow() {
   const win = new BrowserWindow({
     width: 1400,
@@ -23,8 +33,8 @@ menu.append(
     label: "设置",
     type: "checkbox",
     checked: true,
-    click: () => {
-      console.log("设置");
+    click: (e) => {
+      console.log("设置", e);
     },
   })
 );
@@ -35,52 +45,90 @@ menu.append(
     type: "checkbox",
     checked: true,
     click: () => {
-      killBg();
       app.quit();
     },
   })
 );
 
 app.whenReady().then(() => {
-  win = createWindow();
+  mainWindoww = createWindow();
   Menu.setApplicationMenu(menu);
-  win.loadFile("index.html");
-  startBg((err, stdout, stderr) => {
-    if (err) {
-      console.log("startBg", err);
-      return;
-    }
-    win.loadURL("http://127.0.0.1:10081/home");
-  });
+  mainWindoww.loadFile("index.html");
+  setTimeout(() => {
+    mainWindoww.loadURL(bgUrl);
+  }, 3000);
+  startBg(mainWindoww);
   app.on("window-all-closed", function () {
-    if (process.platform !== "darwin") app.quit();
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
   });
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "打开网页",
+      click: () => {
+        sys.exec("start " + bgUrl);
+      },
+    },
+    {
+      label: "显示窗口",
+      click: showThis,
+    },
+    {
+      label: "关闭系统",
+      click: () => {
+        killBg();
+      },
+    },
+  ]);
+  const icon = nativeImage.createFromPath("assert/s.png");
+  const tray = new Tray(icon);
+  tray.setToolTip("This is my application");
+  tray.setTitle("This is my title");
+  tray.setContextMenu(contextMenu);
   app.on("activate", function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
+const showThis = () => {
+  const { app } = require("electron");
+  app.relaunch();
+};
 
-function startBg() {
-  sys.exec("cd src && cd app && start appVue.exe && cd .. && cd ..");
+function startBg(mainWindoww) {
+  sys.exec(
+    "cd src && cd viteApp && start appVite.exe && cd .. && cd ..",
+    (err, stdout, stderr) => {
+      console.log("startBg", err, stdout, stderr);
+      if (err) {
+        console.log("startBg", err);
+        return;
+      }
+      console.log("loadURL");
+      mainWindoww.loadURL(bgUrl);
+    }
+  );
 }
 
 function killBg() {
   const ports = ["10081"];
-  var showProcess = process.platform == "win32" ? "netstat -ano" : "ps aux";
-  sys.exec(showProcess, function (err, stdout, stderr) {
+  const showProcess = process.platform == "win32" ? "netstat -ano" : "ps aux";
+  console.log(ports, showProcess);
+  sys.exec(showProcess, (err, stdout, stderr) => {
+    console.log(err, stdout, stderr);
     if (err) {
       return console.log(err);
     }
     stdout.split("\n").filter(function (line) {
       var p = line.trim().split(/\s+/);
       var address = p[1];
-
       if (address != undefined) {
         if (ports.indexOf(address.split(":")[1]) >= 0) {
           sys.exec("taskkill /F /pid " + p[4], function (err, stdout, stderr) {
             if (err) {
               return console.log("释放指定端口失败！！");
             }
+            app.quit();
             console.log("占用指定端口的程序被成功杀掉！");
           });
         }
