1e50110c15d1372a5a25bbc9baac84f069697af9
hunter <18358821@qq.com>
2023-09-23 19:07:05 +0800

    1

--- quasarhome/quasar.config.js
+++ quasarhome/quasar.config.js
@@ -90,7 +90,7 @@ module.exports = configure(function (/* ctx */) {
       open: true, // opens browser window automatically
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:10081/',
+          target: 'http://localhost:10081/',
           changeOrigin: true,
           pathRewrite: {
             '^/api': '',
--- quasarhome/src-electron/electron-main.ts
+++ quasarhome/src-electron/electron-main.ts
@@ -18,6 +18,7 @@ function createWindow() {
     useContentSize: true,
     webPreferences: {
       contextIsolation: true,
+      webSecurity: false,
       // More info: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/electron-preload-script
       preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD),
     },
@@ -34,6 +35,7 @@ function createWindow() {
       mainWindow?.webContents.closeDevTools();
     });
   }
+  mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', () => {
     mainWindow = undefined;
