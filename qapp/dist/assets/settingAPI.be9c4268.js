20e1e4900faba8a8396da9a0ae7dba26c767bb80
hunterPC <hunter@hnter>
2023-09-26 23:22:50 +0800

    viteapp

--- qapp/dist/assets/settingAPI.be9c4268.js
+++
@@ -1,1 +0,0 @@
-import{axios as s}from"./axios.a5ed1f91.js";const e=async()=>await s.get("/api/buttoms"),o=async t=>{const a=await s.post("/api/setting",t);return a&&a.data},r=async()=>{const t=await s.get("/api/GetIpAddr");return t&&t.data},i=async()=>await s.get("/api/shutDown");export{i as G,o as P,e as a,r as b};
--- qapp/dist/assets/use-quasar.c46293eb.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aq as r}from"./index.79d560a9.js";function u(){return a(r)}export{u};
