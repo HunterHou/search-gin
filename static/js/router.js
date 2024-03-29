783215e5d4600efb26938e3ce0811eb59c99ffad
hunter <hunter@harmay.com>
2024-03-29 18:27:48 +0800

    clean

--- static/js/router.js
+++
@@ -1,9 +0,0 @@
-const routes = [
-    { path: "/home", component: homeVue },
-    { path: "/setting", component: setting },
-    { path: "/menu/:no", component: menu },
-    { path: "/context/:id", component: detail }
-];
-const router = new VueRouter({
-    routes,
-});
