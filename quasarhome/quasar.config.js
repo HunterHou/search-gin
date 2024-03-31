bb0b4266a31c22467264917edc68dc0a0707d8ec
hunterPC <hunter@hnter>
2024-03-31 16:00:49 +0800

    1

--- quasarhome/quasar.config.js
+++ quasarhome/quasar.config.js
@@ -102,7 +102,7 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: { dark: 'auto' },
+      config: {},
       lang: 'zh-CN',
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
