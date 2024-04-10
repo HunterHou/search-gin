c6f700f43cb4e6344fe78ddebb59ee3bdb1ba1ec
hunter <hunter@harmay.com>
2024-04-10 17:59:15 +0800

    1

--- electron/src/stores/System.ts
+++ electron/src/stores/System.ts
@@ -4,19 +4,19 @@ import { SettingInfo } from 'components/model/Setting';
 
 export const useSystemProperty = defineStore({
   id: 'system',
-  // persist: {
-  //   enabled: true,
-  //   // 自定义持久化参数
-  //   strategies: [
-  //     {
-  //       // 自定义key
-  //       key: "systemProperty",
-  //       // 自定义存储方式，默认sessionStorage
-  //       storage: localStorage,
-  //       // 指定要持久化的数据，默认所有 state 都会进行缓存，可以通过 paths 指定要持久化的字段，其他的则不会进行持久化。
-  //     },
-  //   ],
-  // },
+  persist: {
+    enabled: true,
+    // 自定义持久化参数
+    strategies: [
+      {
+        // 自定义key
+        key: "systemProperty",
+        // 自定义存储方式，默认sessionStorage
+        storage: localStorage,
+        // 指定要持久化的数据，默认所有 state 都会进行缓存，可以通过 paths 指定要持久化的字段，其他的则不会进行持久化。
+      },
+    ],
+  },
   state: () => ({
     isFullscreen: false,
     shutdownLeftSecond:null,
