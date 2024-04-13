4e3b9b2971745bbcc1fef9617064930987e6ebb2
hunterPC <hunter@hnter>
2024-04-13 14:00:54 +0800

    1

---
+++ electron_quasar/src-electron/icons/qapp/dist/assets/settingAPI.9566cf86.js
@@ -0,0 +1,1 @@
+import{c as s}from"./axios.99901827.js";const e=async()=>await s().get("/api/buttoms"),o=async t=>{const a=await s().post("/api/setting",t);return a&&a.data},r=async()=>{const t=await s().get("/api/GetIpAddr");return t&&t.data},c=async()=>await s().get("/api/shutDown");export{c as G,o as P,e as a,r as b};
