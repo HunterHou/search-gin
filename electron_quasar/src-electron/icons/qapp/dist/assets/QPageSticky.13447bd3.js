8ab4bb4507334a19a60e56219226d7fd92d6c3cd
hunterPC <hunter@hnter>
2024-04-18 19:38:31 +0800

    1

---
+++ electron_quasar/src-electron/icons/qapp/dist/assets/QPageSticky.13447bd3.js
@@ -0,0 +1,1 @@
+import{E as a}from"./index.a657d947.js";import{u as o,a as r}from"./use-page-sticky.0c5e1c43.js";var i=a({name:"QPageSticky",props:o,setup(s,{slots:t}){const{getStickyContent:e}=r();return()=>e(t)}});export{i as Q};
