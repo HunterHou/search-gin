432656df50c3fa0cbc5ca768fc43101aff9e048b
hunter <hunter@harmay.com>
2024-04-22 15:11:52 +0800

    sync.Map

--- gosrc/utils/Logger.go
+++ gosrc/utils/Logger.go
@@ -12,17 +12,21 @@ var fLog, _ = os.OpenFile("gin.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
 var LogWriter = io.MultiWriter(fLog, os.Stdout)
 
 func InfoFormat(format string, v ...any) {
-	output, _ := json.Marshal(&v)
-	_, err := fmt.Fprintln(gin.DefaultWriter, format+string(output))
-	if err != nil {
-		return
-	}
+	go func() {
+		output, _ := json.Marshal(&v)
+		_, err := fmt.Fprintln(gin.DefaultWriter, format+string(output))
+		if err != nil {
+			return
+		}
+	}()
 }
 
 func InfoNormal(v ...any) {
-	output, _ := json.Marshal(&v)
-	_, err := fmt.Fprintln(gin.DefaultWriter, output)
-	if err != nil {
-		return
-	}
+	go func() {
+		output, _ := json.Marshal(&v)
+		_, err := fmt.Fprintln(gin.DefaultWriter, output)
+		if err != nil {
+			return
+		}
+	}()
 }
